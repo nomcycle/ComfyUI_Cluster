911119fb90543d53b1289c744dd5883bdb391591
Sean Connor <connor.i.sean@gmail.com>
2025-01-09 10:49:10 -0700

    Buffers are propagating through UDP successfully now.

--- states/executing_state.py
+++ states/executing_state.py
@@ -1,11 +1,11 @@
+from typing import Dict, List, TYPE_CHECKING
+
 import asyncio
 import random
-import time
 import numpy as np
 import torch
 import math
 import threading
-from typing import Dict, List, TYPE_CHECKING
 from enum import Enum, auto
 
 from ..log import logger
@@ -23,12 +23,20 @@ from ..env_vars import EnvVars
 
 # Constants
 HEADER_SIZE = 12  # 4 bytes each for buffer flag, chunk id, instance index
-UDP_MTU = 1400
+UDP_MTU = 1460
 
 class ExecutingSubState(Enum):
-    NORMAL = auto()
+    RECEIVING = auto()
+    SENDING = auto()
+
+class ReceiveState(Enum):
+    RECEIVING = auto()
     REQUESTING_RESEND = auto()
-    RESENDING = auto()
+    WAITING_ALL_ACK = auto()
+
+class SendState(Enum):
+    PREAMBLE = auto()
+    SENDING = auto()
 
 class ExecutingStateHandler(StateHandler):
     def __init__(self, instance: ThisInstance):
@@ -43,41 +51,72 @@ class ExecutingStateHandler(StateHandler):
         self._received_buffers: Dict[int, bytes] = {}  # instance_index -> joined buffer
 
         self._chunk_lock = threading.Lock()
-        self._substate = ExecutingSubState.NORMAL
+        self._substate = ExecutingSubState.RECEIVING
+        self._receive_state = ReceiveState.RECEIVING
+        self._send_state = SendState.PREAMBLE
         self._missing_chunk_ids = []
         self._current_distribution_instance_index = 0
+        self._received_acks = set()  # Track which instances have ACKed
         super().__init__(instance, ClusterState.EXECUTING, ClusterMessageType.DISTRIBUTE_BUFFER_BEGIN | ClusterMessageType.DISTRIBUTE_BUFFER_ALL_SENT | ClusterMessageType.DISTRIBUTE_BUFFER_RESEND | ClusterMessageType.DISTRIBUTE_BUFFER_ACK)
         logger.debug("Initialized ExecutingStateHandler")
 
     async def handle_state(self, current_state: int) -> StateResult | None:
-        if self._received_buffers_from_all_instances:
-            logger.debug("All buffers received, sending ACK")
-            message = ClusterDistributeBufferAck()
-            message.header.type = ClusterMessageType.DISTRIBUTE_BUFFER_ACK
-            message.header.require_ack = True
-            message.instance_index = EnvVars.get_instance_index() + 1
-            await self._instance.cluster.udp.send_and_wait(message)
-            return
+        if self._substate == ExecutingSubState.RECEIVING:
+            if self._receive_state == ReceiveState.RECEIVING:
+                if self._received_buffers_from_all_instances:
+                    logger.debug("All buffers received, sending ACK")
+                    message = ClusterDistributeBufferAck()
+                    message.header.type = ClusterMessageType.DISTRIBUTE_BUFFER_ACK
+                    message.header.require_ack = True
+                    message.instance_index = EnvVars.get_instance_index() + 1
+                    await self._instance.cluster.udp.send_and_wait(message)
+                    self._receive_state = ReceiveState.WAITING_ALL_ACK
+                    return
 
-        elif self._substate == ExecutingSubState.REQUESTING_RESEND:
-            resend_msg = ClusterDistributeBufferResend()
-            resend_msg.header.type = ClusterMessageType.DISTRIBUTE_BUFFER_RESEND
-            resend_msg.header.require_ack = True
-            resend_msg.instance_index = EnvVars.get_instance_index()
-            resend_msg.missing_chunk_ids.extend(self._missing_chunk_ids)
-            await self._instance.cluster.udp.send_and_wait_thread_safe(resend_msg)
-            self._substate = ExecutingSubState.NORMAL
-            return
+            elif self._receive_state == ReceiveState.REQUESTING_RESEND:
+                resend_msg = ClusterDistributeBufferResend()
+                resend_msg.header.type = ClusterMessageType.DISTRIBUTE_BUFFER_RESEND
+                resend_msg.header.require_ack = True
+                resend_msg.instance_index = EnvVars.get_instance_index()
+                resend_msg.missing_chunk_ids.extend(self._missing_chunk_ids)
+                await self._instance.cluster.udp.send_and_wait_thread_safe(resend_msg)
+                self._receive_state = ReceiveState.RECEIVING
+                return
 
-        elif self._substate == ExecutingSubState.RESENDING:
-            instance_index = EnvVars.get_instance_index()
-            all_sent_msg = ClusterDistributeBufferAllSent()
-            all_sent_msg.header.type = ClusterMessageType.DISTRIBUTE_BUFFER_ALL_SENT
-            all_sent_msg.header.require_ack = True
-            all_sent_msg.instance_index = instance_index
-            await self._instance.cluster.udp.send_and_wait_thread_safe(all_sent_msg)
-            self._substate = ExecutingSubState.NORMAL
-            return
+        elif self._substate == ExecutingSubState.SENDING:
+            if self._send_state == SendState.PREAMBLE:
+                instance_index = EnvVars.get_instance_index()
+                byte_buffer_size = len(self._current_byte_buffer)
+                chunk_count = math.ceil(byte_buffer_size / (UDP_MTU - HEADER_SIZE))
+                chunk_ids = [self._generate_key() for _ in range(chunk_count)]
+
+                # Prepare distribution message
+                message = ClusterDistributeBufferBegin(
+                    header=ClusterMessageHeader(
+                        type=ClusterMessageType.DISTRIBUTE_BUFFER_BEGIN,
+                        require_ack=True
+                    ),
+                    instance_index=instance_index,
+                    buffer_type=self._current_buffer_type,
+                    buffer_byte_size=byte_buffer_size,
+                    chunk_count=chunk_count,
+                    chunk_ids=chunk_ids
+                )
+
+                logger.debug("Sending buffer begin message")
+                await self._instance.cluster.udp.send_and_wait_thread_safe(message)
+                self._send_state = SendState.SENDING
+                self._send_chunks(chunk_count, chunk_ids, self._current_byte_buffer)
+                return
+
+            elif self._send_state == SendState.SENDING:
+                instance_index = EnvVars.get_instance_index()
+                all_sent_msg = ClusterDistributeBufferAllSent()
+                all_sent_msg.header.type = ClusterMessageType.DISTRIBUTE_BUFFER_ALL_SENT
+                all_sent_msg.header.require_ack = True
+                all_sent_msg.instance_index = instance_index
+                await self._instance.cluster.udp.send_and_wait_thread_safe(all_sent_msg)
+                return
             
         await asyncio.sleep(0.1)
         return None
@@ -91,6 +130,8 @@ class ExecutingStateHandler(StateHandler):
                 self._expected_buffer_type = distribute_buffer.buffer_type
                 self._expected_chunk_ids = distribute_buffer.chunk_ids
                 self._received_chunks = {}
+                self._substate = ExecutingSubState.RECEIVING
+                self._receive_state = ReceiveState.RECEIVING
 
         elif msg_type == ClusterMessageType.DISTRIBUTE_BUFFER_ALL_SENT:
             total_chunks, expected_total = self._buffer_progress()
@@ -111,11 +152,14 @@ class ExecutingStateHandler(StateHandler):
                             self._missing_chunk_ids.append(chunk_id)
                 
                 logger.warning(f"Missing {len(self._missing_chunk_ids)} chunks, requesting resend")
-                self._substate = ExecutingSubState.REQUESTING_RESEND
+                self._receive_state = ReceiveState.REQUESTING_RESEND
 
         elif msg_type == ClusterMessageType.DISTRIBUTE_BUFFER_RESEND:
+            if self._substate != ExecutingSubState.SENDING:
+                return
             resend_msg = ParseDict(message, ClusterDistributeBufferResend())
             logger.debug(f"Received resend request for {len(resend_msg.missing_chunk_ids)} chunks")
+            
             # Resend requested chunks
             instance_index = EnvVars.get_instance_index()
             common_chunk_byte_header = instance_index.to_bytes(4, byteorder='big')
@@ -124,15 +168,15 @@ class ExecutingStateHandler(StateHandler):
             for i, chunk_id in enumerate(resend_msg.missing_chunk_ids):
                 chunk_id_bytes = chunk_id.to_bytes(4, byteorder='big')
                 chunk_data = self._sent_chunks[chunk_id]
-                # logger.debug(f"Resending chunk {i+1}/{len(resend_msg.missing_chunk_ids)}: chunk_id={chunk_id}")
                 self._emit_byte_chunk(buffer_flag, chunk_id_bytes, common_chunk_byte_header, chunk_data, addr)
             
-            self._substate = ExecutingSubState.RESENDING
         elif msg_type == ClusterMessageType.DISTRIBUTE_BUFFER_ACK:
-            logger.debug("Received buffer ACK, transitioning to IDLE state")
+            logger.debug("Received buffer ACK")
             ack_msg = ParseDict(message, ClusterDistributeBufferAck())
-            self._current_distribution_instance_index = self._current_distribution_instance_index + 1
-            if EnvVars.get_instance_index() > self._current_distribution_instance_index:
+            self._received_acks.add(ack_msg.instance_index)
+            
+            if self._received_buffers_from_all_instances and len(self._received_acks) == EnvVars.get_instance_count() - 1:
+                logger.debug("Received ACKs from all instances, transitioning to IDLE state")
                 from .idle_state import IdleStateHandler
                 return StateResult(current_state, self, ClusterState.IDLE, IdleStateHandler(self._instance))
             return None
@@ -146,7 +190,8 @@ class ExecutingStateHandler(StateHandler):
     def handle_buffer(self, current_state: int, byte_buffer, addr: str) -> StateResult | None:
         if self._received_buffers_from_all_instances:
             logger.error("Received buffer after all buffers already received")
-            raise Exception("Already received buffers from all instances")
+            return
+            # raise Exception("Already received buffers from all instances")
 
         if not byte_buffer or len(byte_buffer) < 12:  # Need at least 12 bytes (4 for flag + 4 for instance index + 4 for chunk id)
             logger.error("Received invalid buffer - too short")
@@ -209,36 +254,10 @@ class ExecutingStateHandler(StateHandler):
             logger.debug(f"Awaiting our turn to distribute. Instance index currently distributing: {self._current_distribution_instance_index}, our instance index: {instance_index}")
             await asyncio.sleep(0.5)
             
-        byte_buffer_size = len(byte_buffer)
-        chunk_count = math.ceil((HEADER_SIZE + byte_buffer_size) / UDP_MTU)
-        chunk_ids = [self._generate_key() for _ in range(chunk_count)]
-        
-        logger.info(f"Distributing buffer: size={byte_buffer_size} bytes, chunks={chunk_count}")
-
-        # Prepare distribution message
-        message = ClusterDistributeBufferBegin(
-            header=ClusterMessageHeader(
-                type=ClusterMessageType.DISTRIBUTE_BUFFER_BEGIN,
-                require_ack=True
-            ),
-            instance_index=instance_index,
-            buffer_type=buffer_type,
-            buffer_byte_size=byte_buffer_size,
-            chunk_count=chunk_count,
-            chunk_ids=chunk_ids
-        )
-
-        logger.debug("Sending buffer begin message")
-        await self._instance.cluster.udp.send_and_wait_thread_safe(message)
-        self._send_chunks(chunk_count, chunk_ids, byte_buffer)
-
-        # Send all sent message
-        logger.debug("Sending all chunks sent message")
-        all_sent_msg = ClusterDistributeBufferAllSent()
-        all_sent_msg.header.type = ClusterMessageType.DISTRIBUTE_BUFFER_ALL_SENT
-        all_sent_msg.header.require_ack = True
-        all_sent_msg.instance_index = instance_index
-        await self._instance.cluster.udp.send_and_wait_thread_safe(all_sent_msg)
+        self._current_byte_buffer = byte_buffer
+        self._current_buffer_type = buffer_type
+        self._substate = ExecutingSubState.SENDING
+        self._send_state = SendState.PREAMBLE
 
         # Wait for buffers with timeout
         # timeout = 30  # 30 second timeout
